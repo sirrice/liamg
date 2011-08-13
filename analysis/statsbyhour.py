80783e5d8ce4af525a0846d1b882c2678c6afc9e
eugene <sirrice@gmail.com>
2011-08-13 14:30:20 -0400

    commented out matplotlib

--- analysis/statsbyhour.py
+++ analysis/statsbyhour.py
@@ -1,11 +1,68 @@
 from dateutil.parser import parse
 from datetime import timedelta, datetime, time, date
 import sqlite3, math
-import numpy as np
+#import numpy as np
 import sys
 
 
+class Scatter(object):
+    def proc_rows(self, res):
+        return map(tuple, map(lambda row: map(float, row), res))
 
+
+    def get_data(self, queries):
+        conn = sqlite3.connect('../mail.db', detect_types=sqlite3.PARSE_DECLTYPES)
+
+        cur = conn.cursor()
+        colors = ['r-', 'b-', 'o-', 'y-', 'g-']
+
+        data = {}
+        labels = []
+
+        i = 0
+        for title, sql in queries:
+
+            res = cur.execute(sql)
+            res = self.proc_rows(res)
+
+            points = []
+            d = {}
+            for val, hour in res:
+                d[hour] = val
+
+            for hour in xrange(24):
+                if hour in d:
+                    points.append(d[hour])
+                else:
+                    points.append(0)
+                    
+            xs = [datetime(2000, 1, 1, hour) for hour in xrange(24)]                    
+
+            data[title] = points
+            if not labels :
+                labels = map(str, xs)
+
+        cur.close()
+        data['labels'] = labels
+        return data
+        
+    def viz(self, data):
+        xs = data['labels']
+
+        maxy = max([max(avgs) for name, avgs in data.items() if name != 'labels'])
+        for name, avgs in data.items():
+            if name == 'labels': continue
+            print name
+            plt.cla()        
+            plt.clf()
+            xs = [x for x in xrange(len(xs))]
+
+            plt.scatter(xs, avgs, c='r')
+            plt.show()
+            plt.draw()
+            if 'x' in sys.stdin.readline():
+                break
+        
 
 class LineData(object):
 
@@ -13,7 +70,7 @@ class LineData(object):
         return map(tuple, map(lambda row: map(float, row), res))
 
     def get_data(self, queries):
-        conn = sqlite3.connect('./mail.db', detect_types=sqlite3.PARSE_DECLTYPES)
+        conn = sqlite3.connect('../mail.db', detect_types=sqlite3.PARSE_DECLTYPES)
 
         cur = conn.cursor()
 
@@ -68,7 +125,7 @@ class LineData(object):
             
 
 
-class RepliesByHour(LineData):
+class RepliesByHour(object):
     def get_sql(self, lat=True, reply=True, start=None, end = None, daysofweek = None, name="eugene"):
         WHERE = []
         WHERE.append("l.lat < 1")
@@ -98,7 +155,7 @@ class RepliesByHour(LineData):
         return sql
 
 
-class AllByHour(LineData):
+class EveryoneByHour(object):
     def get_sql(self, lat=True, start=None, end = None, daysofweek = None, name="eugene"):
         WHERE = []
         WHERE.append("l.lat < 1")
@@ -118,15 +175,40 @@ class AllByHour(LineData):
 
         sql = "SELECT %s FROM latency l WHERE %s GROUP BY hour ORDER BY hour asc;" % (SELECT, WHERE)
         return sql
+
+
+class AllByHour(object):
+    def get_sql(self, lat=True, start=None, end = None, daysofweek = None, name="eugene"):
+        "select *, strftime('%H', date) from msgs"
+        WHERE = []
+        WHERE.append("l.lat < 1")
+        if start:
+            WHERE.append("datetime(sentdate) > datetime('%s')" % start.strftime('%Y-%m-%d'))
+        if end:
+            WHERE.append("datetime(sentdate) < datetime('%s')" % end.strftime('%Y-%m-%d'))
+        if daysofweek:
+            WHERE.append("strftime('%w', sentdate) in []" % (map(lambda x: "'%s'" % x, daysofweek)))
+
+        if lat:
+            SELECT = "avg(lat) as avglat, strftime('%H', sentdate) as hour"#, CAST(strftime('%M', sentdate)/60 as integer) as minute"
+        else:
+            SELECT = "count(*), strftime('%H', sentdate) as hour"#, CAST(strftime('%M', sentdate)/60 as integer) as minute"
+
+        WHERE = ' and '.join(WHERE)
+
+        sql = "SELECT %s FROM latency l WHERE %s GROUP BY hour ORDER BY hour asc;" % (SELECT, WHERE)
+        return sql
+
+
     
 
 
 if __name__ == '__main__':
-    import matplotlib.pyplot as plt
+#    import matplotlib.pyplot as plt
     import json
-    plt.ion()
+#    plt.ion()
 
-    stats = AllByHour()
+    genqs = EveryoneByHour()
     queries = []
 
     for month in xrange(12):
@@ -134,10 +216,11 @@ if __name__ == '__main__':
         start = date(2010, month, 1)
         end = start + timedelta(days=30)
         #queries.append(('Count 2011 %d' % month, stats.get_sql(lat=False, reply=None, start=start, end = end)))
-        queries.append(('Count 2011 %d' % month, stats.get_sql(lat=True, start=start, end = end)))
-    data = stats.get_data(queries)
-    stats.viz(data)
+        queries.append(('Count 2011 %d' % month, genqs.get_sql(lat=True, start=start, end = end)))
 
+    foo = Scatter()
+    data = foo.get_data(queries)
+#    foo.viz(data)
     print json.dumps(data)
         
         
