54a737c549a8adde0be923f466471869abc970cb
eugene <sirrice@gmail.com>
2011-08-13 21:05:36 -0400

    added media/ folder and added week/day stats functionality

--- liamgwebapp/urls.py
+++ liamgwebapp/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(    '',
     (r'^emailanalysis/', include('emailanalysis.urls')),
-    # Examples:
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve',  {'document_root': settings.MEDIA_ROOT })
+     # Examples:
     # url(r'^$', 'liamgwebapp.views.home', name='home'),
     # url(r'^liamgwebapp/', include('liamgwebapp.foo.urls')),
 
