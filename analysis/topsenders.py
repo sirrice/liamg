6284f09158454a0cc7c28ce2e9cbd97dba601d70
Christopher Farm <farm.cp@gmail.com>
2011-08-23 01:49:55 -0400

    update topsenders

--- analysis/topsenders.py
+++ analysis/topsenders.py
@@ -26,7 +26,7 @@ def get_top_senders(num, startdate, enddate, user, conn):
         dateStr = "and date >= '" + startdate + "' and date < '" + enddate + "'"
 
         execCode = 'select email, count(*) as c from emails, contacts where emails.fr = contacts.id %s %s group by email order by c desc limit %d;' % (emailStr, dateStr, num)
-        print execCode
+
         c.execute(execCode)
         res = c.fetchall()
     except Exception, e:
