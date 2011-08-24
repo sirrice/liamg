9b6b0cb35e950627094f36f7798236417b148845
Christopher Farm <farm.cp@gmail.com>
2011-08-24 01:15:14 -0400

    fixed sql query for topsent

--- analysis/topsenders.py
+++ analysis/topsenders.py
@@ -16,17 +16,17 @@ def get_top_senders(num, startdate, enddate, user, conn):
         
         #might need a better way to do this -- if we have a business account there will be other email strings that we can't account for. However if we include
         #everything then we will not be able to filter out the spam that people send to the user
-        email_list = ["'%yahoo%'", "'%gmail%'", "'%aol%'", "'%hotmail%'", "'%live.com%'"]
+        #email_list = ["'%yahoo%'", "'%gmail%'", "'%aol%'", "'%hotmail%'", "'%live.com%'"]
         #for now going to use the senders list to predict who is important -> a person wouldn't send an email unless they were on the senders list
 
-#        email_list = topsent.get_emails_topsent(startdate, enddate, user, conn)        
-#        email_list = ["'%{0}%'".format(email) for email in email_list]
+        email_list = topsent.get_emails_topsent(startdate, enddate, user, conn)      
+        email_list = ["'%{0}%'".format(email) for email in email_list]
 
         emailStr = "and (email like " + " or email like ".join(email_list) + ")"
         dateStr = "and date >= '" + startdate + "' and date < '" + enddate + "'"
 
         execCode = 'select email, count(*) as c from emails, contacts where emails.fr = contacts.id %s %s group by email order by c desc limit %d;' % (emailStr, dateStr, num)
-
+        print execCode
         c.execute(execCode)
         res = c.fetchall()
     except Exception, e:
